af26e40058ae2311e7cbc92a43674c9e66abd20c
Dhruv <good-times-ahead@users.noreply.github.com>
2022-02-09 15:55:15 +0530

    feat: add indexes for faster queries

--- app/core/models.py
+++ app/core/models.py
@@ -4,5 +4,11 @@ from django.db import models
 # Create your models here.
 class TodoItem(models.Model):
     task = models.CharField(max_length=200)
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField("Added At")
     completed = models.BooleanField(default=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["completed", "added_at"]),
+            models.Index(fields=["added_at"]),
+        ]
