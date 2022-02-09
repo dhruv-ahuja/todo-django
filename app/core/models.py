b127b0e4f4563c488dec973000cbef10f1b78a0d
Dhruv <good-times-ahead@users.noreply.github.com>
2022-02-09 13:16:08 +0530

    feat: add database model & make migrations

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class TodoItem(models.Model):
+    task = models.CharField(max_length=200)
+    added_at = models.DateTimeField()
+    completed = models.BooleanField(default=False)
