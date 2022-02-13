e4302f5adac34e367e04dd617d1ddf1fbc051a52
Dhruv <dhruvahuja2k@gmail.com>
2022-02-13 17:19:02 +0530

    fix: add missing urls

--- app/core/urls.py
+++ app/core/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("delete/<int:pk>", views.confirm_deletion, name="confirm_deletion"),
+    path("confirm/<int:pk>", views.confirm_deletion, name="confirm_deletion"),
+    path("delete/<int:pk>", views.delete_todo, name="delete_todo"),
     path("update/<int:pk>", views.complete_todo, name="complete_todo"),
 ]
