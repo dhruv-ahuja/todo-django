be5123b6885b37045ad62cd4912dd8f929f01b43
Dhruv <dhruvahuja2k@gmail.com>
2022-02-14 16:38:16 +0530

    refactor: change database model
    - prepration to add user authentication system to the application

--- app/core/models.py
+++ app/core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class TodoItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     task = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.task
@@ -16,4 +17,4 @@ class TodoItem(models.Model):
             models.Index(fields=["added_at"]),
         ]
 
-        # ordering = ["completed"]
+        ordering = ["completed"]
